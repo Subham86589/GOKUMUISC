652fe7f5da8b88ab5a2c6bef2edb4974ac52ed34
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-17 21:50:00 +0530

    Update play.py
    Signed-off-by: ProGamer097 <156319601+ProGamer097@users.noreply.github.com>

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -26,19 +26,10 @@ from config import BANNED_USERS, lyrical
 
 
 @app.on_message(
-    filters.command(
-        [
-            "play",
-            "vplay",
-            "cplay",
-            "cvplay",
-            "playforce",
-            "vplayforce",
-            "cplayforce",
-            "cvplayforce",
-        ]
-    )
-    & filters.group
+   filters.command(["play", "vplay", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+            
+)           
+    & filters.group(
     & ~BANNED_USERS
 )
 @PlayWrapper
